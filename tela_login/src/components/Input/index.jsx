f9acbc12fad25754f569f052e518b7e4c373d6d4
wastenio silva <wastenio.silva@gmail.com>
2024-01-11 22:38:31 -0300

    Configuração da confirmação do email e senha

--- tela_login/src/components/Input/index.jsx
+++ tela_login/src/components/Input/index.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import { IconContainer, InputContainer, InputText} from './styles';
+import { ErrorText, IconContainer, InputContainer, InputText} from './styles';
+import { Controller } from 'react-hook-form';
 
-const Input = ({leftIcon, name, ...rest}) => {
+const Input = ({leftIcon, name, control, errorMessage, ...rest}) => {
   return (
-    <InputContainer>
-    {leftIcon ? (<IconContainer>{leftIcon}</IconContainer>) : null}
-      <InputText {...rest}/>
-    </InputContainer>
+    <>
+      <InputContainer>
+      {leftIcon ? (<IconContainer>{leftIcon}</IconContainer>) : null}
+      <Controller
+      name={name}
+      control={control}
+      rules={{required: true}}
+      render={({field}) => <InputText {...field} {...rest}/>}
+      />
+      </InputContainer>
+      {errorMessage ? <ErrorText>{errorMessage}</ErrorText> : null}
+    </>
   )
 }
 
--- tela_login/src/pages/login/index.jsx
+++ tela_login/src/pages/login/index.jsx
@@ -1,19 +1,47 @@
 import { MdEmail } from 'react-icons/md';
 import { CiLock } from "react-icons/ci";
 import { useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
 import { Button } from '../../components/Button';
 import { Header } from '../../components/Header';
 import { Input } from '../../components/Input';
+import { api } from '../../services/api';
+import { Column, Container, Criartext, EsqueciText, Row, SubtitleLogin, Title, TitleLogin, Wrapper } from './styles';
 
-import { Column, Container, Criartext, EsqueciText, Row, SubtitleLogin, Title, TitleLogin, Wrapper } from './atyles';
+const schema = yup
+  .object({
+    email: yup.string().email('email não é valido').required('Campo obrigatório'),
+    password: yup.string().min(3, 'No minimo 3 caracteres').required('Campo obrigatório'),
+  })
+  .required()
 
 const Login = () => {
 
     const navigate = useNavigate();
 
-    const handleClickSignIn = () => {
-        navigate('/feed')
-    }
+    const { control, handleSubmit, formState: { errors } } = useForm({
+        resolver: yupResolver(schema),
+        mode: 'onChange',
+    });
+
+    const onSubmit = async (formData) => {
+        try{
+            const {data} = await api.get(`/users?email=${formData.email}&senha=${formData.senha}`);
+            
+            if(data.length && data[0].id){
+                navigate('/feed') 
+                return
+            }
+
+            alert('Usuário ou senha inválido')
+        }catch(e){
+            //TODO: HOUVE UM ERRO
+        }
+    };
+
+    console.log('errors', errors);
 
     return(<>
         <Header />
@@ -25,19 +53,19 @@ const Login = () => {
                 </Title>
             </Column>
             <Column>
-            <Wrapper>
-                <TitleLogin>Faça seu cadastro</TitleLogin>
-                <SubtitleLogin>Faça seu cadastro e make the change._</SubtitleLogin>
-                <form>
-                    <Input placeholder="E-mail" leftIcon={<MdEmail />}/>
-                    <Input placeholder="Senha" type="password" leftIcon={<CiLock />} />
-                    <Button title="Entrar" variant="secondary" onClick={handleClickSignIn} type="button"/>
-                </form>
-                <Row>
-                    <EsqueciText>Esqueci minha senha</EsqueciText>
-                    <Criartext>Criar conta</Criartext>
-                </Row>
-            </Wrapper>
+                <Wrapper>
+                    <TitleLogin>Faça seu cadastro</TitleLogin>
+                    <SubtitleLogin>Faça seu cadastro e make the change._</SubtitleLogin>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <Input name="email" errorMessage={errors?.email?.message} control={control} placeholder="E-mail" leftIcon={<MdEmail />}/>
+                        <Input name="password" errorMessage={errors?.password?.message} control={control} placeholder="Senha" type="password" leftIcon={<CiLock />} />
+                        <Button title="Entrar" variant="secondary" type="submit"/>
+                    </form>
+                    <Row>
+                        <EsqueciText>Esqueci minha senha</EsqueciText>
+                        <Criartext>Criar conta</Criartext>
+                    </Row>
+                </Wrapper>
             </Column>
         </Container>
     </>)
